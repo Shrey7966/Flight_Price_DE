3b5984f04a4cb955c5f3716038417a94d3669459
Shrey7966 <94635069+Shrey7966@users.noreply.github.com>
2025-03-28 01:00:00 +0530

    Delete scripts/upload_to_s3.py
    not required as its been merged with another file

--- scripts/upload_to_s3.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-s3 = boto3.client("s3")
-BUCKET_NAME = "flight-price-etl-github"
-
-s3.upload_file("data/flight_prices.json", BUCKET_NAME, "flight_prices.json")
-print("Data uploaded to S3 successfully.")
